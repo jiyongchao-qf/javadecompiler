6683fd7c0f6afc506ac04dad01dbf63dbecbe94a
吉永超 <jiyongchaowy@163.com>
2021-03-01 20:40:47 +0800

    目录调整

--- src/main/java/com/java/decompiler/javadecompiler/JavaDecompilerApplication.java
+++ src/main/java/com/java/decompiler/javadecompiler/JavaDecompilerApplication.java
@@ -3,11 +3,14 @@ package com.java.decompiler.javadecompiler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author jiyongchao
+ */
 @SpringBootApplication
-public class JavadecompilerApplication {
+public class JavaDecompilerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavadecompilerApplication.class, args);
+        SpringApplication.run(JavaDecompilerApplication.class, args);
     }
 
 }
