25eb9444066ec228e9698ce761c4b8d36ef96c57
吉永超 <jiyongchaowy@163.com>
2021-03-01 19:33:39 +0800

    init

---
+++ src/main/java/com/java/decompiler/JavadecompilerApplication.java
@@ -0,0 +1,13 @@
+package com.java.decompiler.javadecompiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadecompilerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavadecompilerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/decompiler/JavadecompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.decompiler.javadecompiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadecompilerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
